6b19ec1a59fb5efe1d472726819239e8293b668b
Karthik <rajakarthik131@gmail.com>
2022-05-18 21:59:03 +0530

    Add separate model to store company details

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Company
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
     pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 
 class User(AbstractUser):
-    company_name = models.CharField(max_length=1024)
-    vat_id = models.CharField(max_length=255)
+    company = models.ForeignKey("users.Company", on_delete=models.CASCADE, null=True)
+
+
+class Company(TimeStampedModel):
+    name = models.CharField(max_length=1025)
     address = models.TextField()
+    vat_id = models.CharField(max_length=255)
